6b9370868af1976e9a1e7d21b50ae569f39e865f
callee <heweitykc@163.com>
2013-05-12 12:15:09 +0800

    新项目

---
+++ src/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.startbtn = new System.Windows.Forms.Button();
+            this.urltxt = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(2, -16);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(950, 610);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Url = new System.Uri("", System.UriKind.Relative);
+            // 
+            // startbtn
+            // 
+            this.startbtn.Location = new System.Drawing.Point(961, 12);
+            this.startbtn.Name = "startbtn";
+            this.startbtn.Size = new System.Drawing.Size(75, 23);
+            this.startbtn.TabIndex = 1;
+            this.startbtn.Text = "开始";
+            this.startbtn.UseVisualStyleBackColor = true;
+            this.startbtn.Click += new System.EventHandler(this.start_Click);
+            // 
+            // urltxt
+            // 
+            this.urltxt.Font = new System.Drawing.Font("SimSun", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.urltxt.Location = new System.Drawing.Point(2, 600);
+            this.urltxt.Multiline = true;
+            this.urltxt.Name = "urltxt";
+            this.urltxt.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.urltxt.Size = new System.Drawing.Size(1067, 132);
+            this.urltxt.TabIndex = 2;
+            this.urltxt.WordWrap = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(961, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "显示导航";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1081, 732);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.urltxt);
+            this.Controls.Add(this.startbtn);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button startbtn;
+        private System.Windows.Forms.TextBox urltxt;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ src/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        private HtmlElement flashdoc;
+        public const String GAMEHOST = "http://app100645087.qzone.qzoneapp.com/?qz_height=1400&openid=C57315B1BE0C744F34E740A0A836E376&openkey=CD6A6C57748AB3EE211857C09C9FB34D&pf=qzone&pfkey=7db8d5d3290298f74a569660b734ceef&qz_ver=6&appcanvas=1&params=&via=QZ.MYAPP";
+        public Form1()
+        {
+            InitializeComponent();
+            webBrowser1.ScrollBarsEnabled = false;
+            webBrowser1.Visible = false;
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            count = 0;
+            webBrowser1.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(PrintDocument);
+            webBrowser1.ProgressChanged += new WebBrowserProgressChangedEventHandler(onProgress);
+            webBrowser1.Navigated += new WebBrowserNavigatedEventHandler(onNavigated);
+            webBrowser1.Navigate(GAMEHOST);
+            urltxt.AppendText(GAMEHOST + "\n");
+        }
+        private int count = 0;
+        private void PrintDocument(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            urltxt.AppendText(e.Url.ToString()+"\n");
+            count++;
+            if (count >= 5) initbroswer();
+        }
+
+        private void onNavigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            urltxt.AppendText(e.Url.ToString() + "\n");
+        }
+
+        private void onProgress(object sender, WebBrowserProgressChangedEventArgs e)
+        {
+            
+        }
+
+        private void initbroswer()
+        {
+            HtmlElementCollection collection = webBrowser1.Document.Body.Children;
+            flashdoc = collection[2];
+            flashdoc.Children[0].Style = "display:none";
+            flashdoc.Children[1].Style = "display:none";
+            webBrowser1.Visible = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "显示导航")
+            {
+                flashdoc.Children[1].Style = "display:block";
+                button1.Text = "隐藏导航";
+            }  
+            else
+            {
+                flashdoc.Children[1].Style = "display:none";
+                button1.Text = "显示导航";
+            }
+
+        }
+    }
+}
